48581e570e996c347f9724e2122f159c3475c0a5
NHLong203 <m10fcbar@gmail.com>
2023-11-08 19:56:26 +0700

    Long_1

--- database.py
+++
@@ -1,5 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from os import path
-from flask import Flask,url_for,redirect,render_template
-
-
--- index.js
+++ index.js
@@ -10,6 +10,9 @@ let porcion1_c  = document.getElementById("porcion1-c")
 let donut_chart  = document.getElementById("donut-chart")
 let switch_button  = document.getElementsByClassName("switch-button")
 let ctx2 = document.getElementById('myDoubleBarChart').getContext('2d');
+let ctx3 = document.getElementById('lineChartFood').getContext('2d');
+let ctx4 = document.getElementById('lineChartFashion').getContext('2d');
+let ctx5 = document.getElementById('lineChartFilm').getContext('2d');
 const ctx = document.getElementById('myPieChart').getContext('2d');
 
 
@@ -62,6 +65,8 @@ let doubleBarChart = new Chart(ctx2, {
 			}
 		]
 	},
+
+
 	options: {
 		scales: {
 			y: {
@@ -76,6 +81,105 @@ let click_chart = function(data,type) {
 	myPieChart.update();
 }
 
+let click_line_chart = function(data,type) {  
+	lineChart.data.datasets[0].data = [data[type]["pos"], data[type]["neg"]];
+	lineChart.update();
+}
+
+
+
+let lineChart_food = new Chart(ctx3, {
+	type: "line",
+  	data: {
+    labels: [],
+    datasets: [{
+		label: "Positve",
+		fill: false,
+		tension: 0,
+		backgroundColor: "rgba(0, 200, 0, 0.59)",
+		borderColor: "rgba(0, 200, 0, 0.59)",
+		data: []
+    },
+	{
+		label: "Negative",
+		fill: false,
+		tension: 0,
+		backgroundColor: "rgba(236, 6, 6, 0.74)",
+		borderColor: "rgba(236, 6, 6, 0.74)",
+		data: []	
+	}
+
+		]
+  },
+  options: {
+    legend: {display: false},
+	scales: {
+		yAxes: [{ticks: {min: 0, max:100}}],
+	  }
+  }
+});
+
+let lineChart_fashion = new Chart(ctx4, {
+	type: "line",
+  	data: {
+    labels: ["1","2","3","4","5","6","7","8","9","10","11","12"],
+    datasets: [{
+		label: "Positve",
+		fill: false,
+		tension: 0,
+		backgroundColor: "rgba(0, 200, 0, 0.59)",
+		borderColor: "rgba(0, 200, 0, 0.59)",
+		data: []
+    },
+	{
+		label: "Negative",
+		fill: false,
+		tension: 0,
+		backgroundColor: "rgba(236, 6, 6, 0.74)",
+		borderColor: "rgba(236, 6, 6, 0.74)",
+		data: []	
+	}
+
+		]
+  },
+  options: {
+    legend: {display: false},
+	scales: {
+		yAxes: [{ticks: {min: 0, max:100}}],
+	  }
+  }
+});
+
+let lineChart_film = new Chart(ctx5, {
+	type: "line",
+  	data: {
+    labels: ["1","2","3","4","5","6","7","8","9","10","11","12"],
+    datasets: [{
+		label: "Positve",
+		fill: false,
+		tension: 0,
+		backgroundColor: "rgba(0, 200, 0, 0.59)",
+		borderColor: "rgba(0, 200, 0, 0.59)",
+		data: []
+    },
+	{
+		label: "Negative",
+		fill: false,
+		tension: 0,
+		backgroundColor: "rgba(236, 6, 6, 0.74)",
+		borderColor: "rgba(236, 6, 6, 0.74)",
+		data: []	
+	}
+
+		]
+  },
+  options: {
+    legend: {display: false},
+	scales: {
+		yAxes: [{ticks: {min: 0, max:100}}],
+	  }
+  }
+});
 
 
 let check_data = function(data) {  
@@ -110,6 +214,7 @@ let active_PieChart = function(data) {
 }
 active_PieChart(data)
 
+
 let active_doubleBarChart = function(data) {  
 	new_data = check_data(data)
 	new_type = Object.keys(new_data)
@@ -119,6 +224,34 @@ let active_doubleBarChart = function(data) {
 	doubleBarChart.update()
 }
 active_doubleBarChart(data)
+
+let active_lineChart_food = function(data) {  
+	new_data = check_data(data)
+	new_type = Object.keys(new_data)
+	lineChart_food.data.labels= Object.values(new_data).map(value =>  value["month"])
+	lineChart_food.data.datasets[0].data = Object.values(new_data).map(value =>  value["percent_1"])
+	lineChart_food.data.datasets[1].data = Object.values(new_data).map(value =>  value["percent_0"])
+	lineChart_food.update()
+}
+active_lineChart_food(data)
+
+let active_lineChart_fashion = function(data) {  
+	new_data = check_data(data)
+	new_type = Object.keys(new_data)
+	lineChart_fashion.data.datasets[0].data = Object.values(new_data).map(value =>  value["pos"])
+	lineChart_fashion.data.datasets[1].data = Object.values(new_data).map(value =>  value["neg"])
+	lineChart_fashion.update()
+}
+active_lineChart_fashion(data)
+
+let active_lineChart_film = function(data) {  
+	new_data = check_data(data)
+	new_type = Object.keys(new_data)
+	lineChart_film.data.datasets[0].data = Object.values(new_data).map(value =>  value["pos"])
+	lineChart_film.data.datasets[1].data = Object.values(new_data).map(value =>  value["neg"])
+	lineChart_film.update()
+}
+active_lineChart_film(data)
 
 
 fileInput.addEventListener("change", function( event ) {  
@@ -141,23 +274,10 @@ fileInput.addEventListener("change", function( event ) {
 			// alert( "success" );
 			console.log("Success")
 			console.log(result)
-			result = {
-				"Fashion":{
-					"pos":10,
-					"neg":20
-				},
-				"Film":{
-					"pos":30,
-					"neg":20
-				},
-				"Food":{
-					"pos":100,
-					"neg":20
-				}
-			}
+		
 
-
-			update_new_pie_chart(result)
+			active_PieChart(result)
+			active_doubleBarChart(result)
 	
 		  })
 		  .fail(function() {
